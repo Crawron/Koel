5f99c4e8e9c38c6f39129fcbe470acc073686967
Crawron <azertyaaron@gmail.com>
2021-10-15 21:24:13 -0500

    ;

--- src/commands/leave.ts
+++ src/commands/leave.ts
@@ -2,15 +2,15 @@ import { Gatekeeper } from "@itsmapleleaf/gatekeeper"
 import { destroyPlayer, tryGetPlayer } from "../playerHandler"
 
 export default function defineCommands(gatekeeper: Gatekeeper) {
-    gatekeeper.addSlashCommand({
-        name: "leave",
-        description: "Leave the current voice channel",
-        run(ctx) {
-            const player = tryGetPlayer(ctx)
-            if (!player) return
+	gatekeeper.addSlashCommand({
+		name: "leave",
+		description: "Leave the current voice channel",
+		run(ctx) {
+			const player = tryGetPlayer(ctx)
+			if (!player) return
 
-            destroyPlayer(ctx.guild?.id ?? "")
-            ctx.reply(() => "_:wave:_")
-        },
-    });
+			destroyPlayer(ctx.guild?.id ?? "")
+			ctx.reply(() => "_:wave:_")
+		},
+	})
 }
