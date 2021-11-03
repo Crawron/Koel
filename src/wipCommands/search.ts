d0a68216b52df9a67ba554115de338d4a0e09659
Crawron <azertyaaron@gmail.com>
2021-11-03 18:05:01 -0500

    i'll deal with this search command later

--- src/wipCommands/search.ts
+++ src/wipCommands/search.ts
@@ -1,74 +1,69 @@
-import { selectMenuComponent, Gatekeeper } from "@itsmapleleaf/gatekeeper"
-import { MessageSelectOptionData } from "discord.js"
-import {
-	accentButton,
-	escFmting,
-	fmtTime,
-	getQueueAddedMessage,
-	grayButton,
-} from "../helpers"
-import { tryGetQueue } from "../queueHandler"
-import { Song } from "../Song"
+// import { selectMenuComponent, Gatekeeper } from "@itsmapleleaf/gatekeeper"
+// import { MessageSelectOptionData } from "discord.js"
+// import {
+// 	accentButton,
+// 	escFmting,
+// 	fmtTime,
+// 	getQueueAddedMessage,
+// 	grayButton,
+// } from "../helpers"
+// import { tryGetQueue } from "../queueHandler"
+// import { Song } from "../Song"
 
-export default function defineCommands(gatekeeper: Gatekeeper) {
-	gatekeeper.addSlashCommand({
-		name: "search",
-		description: "Search YouTube for a video then queue it",
-		options: {
-			query: { type: "STRING", description: "Search query", required: true },
-		},
-		async run(ctx) {
-			const player = tryGetQueue(ctx)
-			if (!player) return
+// export default function defineCommands(gatekeeper: Gatekeeper) {
+// 	gatekeeper.addSlashCommand({
+// 		name: "search",
+// 		description: "Search YouTube for a video then queue it",
+// 		options: {
+// 			query: { type: "STRING", description: "Search query", required: true },
+// 		},
+// 		async run(ctx) {
+// 			const player = tryGetQueue(ctx)
+// 			if (!player) return
 
-			ctx.defer()
+// 			ctx.defer()
 
-			const { query } = ctx.options
-			const songs = await Song.fromRequest(query, ctx.user.id)
+// 			const { query } = ctx.options
+// 			const songs = await Song.requestYtdl(query)
 
-			if (false /* songs.length < 1 */) {
-				ctx.reply(() => `Couldn't find anything for \`${query}\``)
-				return
-			}
+// 			let selected: string[] = ["0"]
 
-			let selected: string[] = ["0"]
+// 			const options = songs.map(
+// 				(video, i): MessageSelectOptionData => ({
+// 					label: video.title,
+// 					description: `[${fmtTime(video.duration)}] ${video.author}`,
+// 					value: i.toString(),
+// 				})
+// 			)
 
-			const options = songs.map(
-				(video, i): MessageSelectOptionData => ({
-					label: video.title,
-					description: `[${fmtTime(video.duration)}] ${video.author}`,
-					value: i.toString(),
-				})
-			)
+// 			const selectedSongs: Song[] = []
 
-			const selectedSongs: Song[] = []
+// 			const replyMessage = ctx.reply(() => {
+// 				if (selectedSongs.length < 1) {
+// 					return [
+// 						`**Results for \`${escFmting(query)}\`**`,
+// 						"> Pick songs to **Add to Queue**. _You can pick one or more \\💙_",
+// 						selectMenuComponent({
+// 							options,
+// 							maxValues: options.length,
+// 							selected,
+// 							onSelect: ({ values }) => (selected = values),
+// 						}),
+// 						accentButton("Add to Queue", () => {
+// 							selectedSongs.push(
+// 								...selected
+// 									.map((v) => songs[parseInt(v)])
+// 									.filter((song): song is Song => !!song)
+// 							)
 
-			const replyMessage = ctx.reply(() => {
-				if (selectedSongs.length < 1) {
-					return [
-						`**Results for \`${escFmting(query)}\`**`,
-						"> Pick songs to **Add to Queue**. _You can pick one or more \\💙_",
-						selectMenuComponent({
-							options,
-							maxValues: options.length,
-							selected,
-							onSelect: ({ values }) => (selected = values),
-						}),
-						accentButton("Add to Queue", () => {
-							selectedSongs.push(
-								...selected
-									.map((v) => songs[parseInt(v)])
-									.filter((song): song is Song => !!song)
-							)
-
-							player.addToQueue(...selectedSongs)
-						}),
-						grayButton("Cancel", () => replyMessage.delete()),
-					]
-				} else {
-					return getQueueAddedMessage(...selectedSongs)
-				}
-			})
-		},
-	})
-}
+// 							player.addToQueue(...selectedSongs)
+// 						}),
+// 						grayButton("Cancel", () => replyMessage.delete()),
+// 					]
+// 				} else {
+// 					return getQueueAddedMessage(...selectedSongs)
+// 				}
+// 			})
+// 		},
+// 	})
+// }
