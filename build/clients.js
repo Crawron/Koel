cce2b1ebe53ad679c3e5980f8f7fe719130cab65
Crawron <azertyaaron@gmail.com>
2021-11-04 17:13:46 -0500

    Delete build files

--- build/clients.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.djsClient = void 0;
-const discord_js_1 = require("discord.js");
-exports.djsClient = new discord_js_1.Client({
-    intents: ["GUILD_VOICE_STATES", "GUILDS"],
-});
-//# sourceMappingURL=clients.js.map
