6ed84414158da6ec60d0b81329303f159ac87026
Crawron <azertyaaron@gmail.com>
2021-11-26 18:19:11 -0500

    update /about

--- src/commands/about.ts
+++ src/commands/about.ts
@@ -17,9 +17,9 @@ export default async function defineCommands(gatekeeper: Gatekeeper) {
 				embedComponent({
 					description: `
 :feather: _**Koel** ~ <@109677308410875904>'s personal music bot_
-_\`All good music bots died so I made my own\`_
+_\`Our big music bots died so I made my own\`_
 
-Give me YouTube, Soundcloud, Bandcamp, Twitter Videos, File URLs, F'in PNGs [idc](http://ytdl-org.github.io/youtube-dl/supportedsites.html "*too* complete list of supported sites")... (not spotify tho)
+Give me YouTube, Soundcloud, Bandcamp, Twitter Videos, File URLs, F'in PNGs [idc](http://ytdl-org.github.io/youtube-dl/supportedsites.html "*too* complete list of supported sites")... (not spotify tho, [bearger](https://bearger.app/) does tho)
 
 **Koel's a private bot, so invitiation requires <@109677308410875904> to also be invited**
 
