b0973084a26b2545985e5dc5111b1f01060f1827
Crawron <azertyaaron@gmail.com>
2021-11-30 15:23:03 -0500

    remove ported code from old messageHelpers

--- src/messageHelpers.ts
+++ src/messageHelpers.ts
@@ -4,8 +4,6 @@ import {
 	embedComponent,
 	ReplyComponent,
 } from "@itsmapleleaf/gatekeeper"
-import { cap, escFmting, fmtTime, isTruthy, paginate } from "./helpers"
-import { Queue } from "./Queue"
 import { Song } from "./modules/Song"
 
 export function getQueueAddedMessage(
@@ -28,48 +26,6 @@ export function getQueueAddedMessage(
 			description: `${songList}`,
 			color: 0x0773e6,
 			footer: footer,
-		}),
-	]
-}
-
-export function getNowPlayingMessage(queue: Queue): ReplyComponent[] {
-	let description = ""
-
-	if (queue.paused) description += "⏸️ "
-
-	const song = queue.currentSong
-	if (!song)
-		return [
-			embedComponent({
-				color: 0x0774e6,
-				description: description + "_Nothing's playin_",
-			}),
-		]
-
-	if (song.uploader) description += `_${escFmting(song.uploader)}_`
-	description += `\n**[${escFmting(song.title)}](${song.pageUrl})**`
-
-	const duration = [
-		queue.currentTime && fmtTime(queue.currentTime),
-		(song.duration && fmtTime(song.duration)) || "??:??",
-	]
-		.filter(isTruthy)
-		.join(" / ")
-	if (duration) description += `\n\`${duration}\``
-
-	description += ` _<@${song.requester}>_`
-
-	return [
-		embedComponent({
-			description,
-			thumbnail: { url: song.thumbnailUrl },
-			color: 0x0774e6,
-			fields: [
-				song.chapters.length > 0 && {
-					name: `${song.chapters.length} Chapters`,
-					value: song.getFormattedChapters(queue.currentTime),
-				},
-			].filter(isTruthy),
 		}),
 	]
 }
