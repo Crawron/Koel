c2c89dc2fae6e0fb7b3b6be8121f72c87275ab70
Crawron <azertyaaron@gmail.com>
2021-11-30 15:20:36 -0500

    Add /skip

---
+++ src/commands/definitions/skip.ts
@@ -0,0 +1,29 @@
+import { embedComponent, Gatekeeper } from "@itsmapleleaf/gatekeeper"
+import { cmdName } from "../../helpers"
+import { controllerStore } from "../../stores/controllerStore"
+import { guildCheck } from "../common"
+import { getNowPlayingMessage } from "../messageHelpers"
+
+export default function defineCommands(gatekeeper: Gatekeeper) {
+	gatekeeper.addSlashCommand({
+		name: cmdName("skip"),
+		description: "Skip the current song",
+		aliases: ["s"],
+		options: {
+			amount: {
+				type: "INTEGER",
+				description:
+					"Amount of songs to skip. Allows negative to go back in queue",
+			},
+		},
+		run: (ctx) => {
+			if (!guildCheck(ctx, ctx.guild)) return
+
+			const controller = controllerStore.getOrCreate(ctx.guild.id)
+
+			controller.queue.next()
+
+			ctx.reply(() => getNowPlayingMessage(controller))
+		},
+	})
+}
