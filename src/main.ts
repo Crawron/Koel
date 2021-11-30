2aaf5cc19d9961ba4fdd30dc0a7c920f2f194afa
Crawron <azertyaaron@gmail.com>
2021-11-30 10:04:20 -0500

    Remove extra code from main

--- src/main.ts
+++ src/main.ts
@@ -2,17 +2,11 @@ import "dotenv/config"
 
 import { Gatekeeper } from "@itsmapleleaf/gatekeeper"
 import { log, LogLevel } from "./logging"
-import { bold } from "chalk"
 import path from "path"
 import { djsClient } from "./clients"
-import { loadQueues } from "./queueHandler"
-import { getQueueStorage } from "./storage"
 import { configure } from "mobx"
 
 configure({ enforceActions: "never" })
-
-const commandMode = process.env.COMMANDMODE ?? "guild"
-
 ;(async () => {
 	await Gatekeeper.create({
 		name: "Koel",
@@ -20,16 +14,6 @@ const commandMode = process.env.COMMANDMODE ?? "guild"
 		commandFolder: path.join(__dirname, "commands/definitions"),
 		logging: true,
 	})
-
-	// djsClient.on("ready", async () => {
-	// log(`Ready. Connected to ${bold(djsClient.user?.username)}`)
-	// log(`Using ${bold(commandMode)} commands`)
-
-	// const storage = await getQueueStorage()
-	// log(storage.length, 0)
-	// loadQueues(storage)
-	// log(`Loaded queues`)
-	// })
 
 	djsClient
 		.login(process.env.KOELTOKEN)
