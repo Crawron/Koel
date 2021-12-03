cb245b6f565e9c82ee355cd5cdffa704cf4901c4
Crawron <azertyaaron@gmail.com>
2021-12-03 00:22:20 -0500

    Add /remove

---
+++ src/commands/remove.ts
@@ -0,0 +1,38 @@
+import { embedComponent, Gatekeeper } from "@itsmapleleaf/gatekeeper"
+import { cmdName, escFmting } from "../helpers"
+import { tryGetQueue } from "../queueHandler"
+
+export default function defineCommands(gatekeeper: Gatekeeper) {
+	gatekeeper.addSlashCommand({
+		name: cmdName("remove"),
+		description: "Remove a song in queue",
+		options: {
+			position: {
+				type: "INTEGER",
+				description: "Which song to remove",
+				required: true,
+			},
+		},
+		run(ctx) {
+			const queue = tryGetQueue(ctx)
+			if (!queue) return
+
+			const { position } = ctx.options
+
+			const removed = queue.removeSong(position)
+			if (removed.length === 0) {
+				ctx.reply(() => "No song at that position")
+				return
+			}
+
+			ctx.reply(() =>
+				embedComponent({
+					description: `Removed ${removed
+						.map((song) => `[${escFmting(song.title)}](${song.pageUrl})`)
+						.join(", ")}`,
+					color: 0x0773e6,
+				})
+			)
+		},
+	})
+}
