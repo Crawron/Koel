00d1d135b70e03cb2143c593fdc0bb20727ce92b
Crawron <azertyaaron@gmail.com>
2021-12-12 05:54:49 -0500

    update chalk on logger

--- src/logging.ts
+++ src/logging.ts
@@ -1,11 +1,4 @@
-import {
-	bgBlueBright,
-	bgRedBright,
-	bgWhite,
-	bgYellow,
-	black,
-	gray,
-} from "chalk"
+import chalk from "chalk"
 
 export enum LogLevel {
 	Debug,
@@ -15,10 +8,10 @@ export enum LogLevel {
 }
 
 const levelText = {
-	0: bgWhite` Dbg `,
-	1: bgBlueBright` Inf `,
-	2: bgYellow` Wrn `,
-	3: bgRedBright` Err `,
+	0: chalk.bgWhite.black` Dbg `,
+	1: chalk.bgBlueBright.black` Inf `,
+	2: chalk.bgYellow.black` Wrn `,
+	3: chalk.bgRedBright.black` Err `,
 }
 
 export function log(message: unknown, level = LogLevel.Info): void {
@@ -27,7 +20,7 @@ export function log(message: unknown, level = LogLevel.Info): void {
 	const timestamp = new Date().toLocaleTimeString()
 
 	if (level === LogLevel.Error)
-		console.error(black(levelText[level]), gray(timestamp), message)
-	else console.log(black(levelText[level]), gray(timestamp), message)
+		console.error(levelText[level], chalk.gray(timestamp), message)
+	else console.log(levelText[level], chalk.gray(timestamp), message)
 	// TODO log to discord channel
 }
