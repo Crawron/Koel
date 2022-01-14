3781aa5a99b9e27ed57f6039fe8bfe41b1e5fe11
Crawron <azertyaaron@gmail.com>
2022-01-14 14:45:21 -0500

    main script reorganize, top level await

--- src/main.ts
+++ src/main.ts
@@ -1,29 +1,27 @@
-import "dotenv/config"
-
 import { Gatekeeper } from "@itsmapleleaf/gatekeeper"
-import { log, LogLevel } from "./logging"
-import path from "path"
-import { configure } from "mobx"
-import { filePath } from "./helpers"
 import { Client } from "discord.js"
+import "dotenv/config"
+import { configure as mobxConfigure } from "mobx"
+import path from "path"
+import { filePath } from "./helpers"
+import { log, LogLevel } from "./logging"
 
 export const djsClient = new Client({
 	intents: ["GUILD_VOICE_STATES", "GUILDS"],
 })
 
-configure({ enforceActions: "never" })
-;(async () => {
-	await Gatekeeper.create({
-		name: "Koel",
-		client: djsClient,
-		commandFolder: path.join(
-			filePath(import.meta.url).dirname,
-			"commands/definitions"
-		),
-		logging: true,
-	})
+mobxConfigure({ enforceActions: "never" })
 
-	djsClient
-		.login(process.env.KOELTOKEN)
-		.catch((e: Error) => log(e, LogLevel.Error))
-})()
+await Gatekeeper.create({
+	name: "Koel",
+	client: djsClient,
+	commandFolder: path.join(
+		filePath(import.meta.url).dirname,
+		"commands/definitions"
+	),
+	logging: true,
+})
+
+djsClient
+	.login(process.env.KOELTOKEN)
+	.catch((e: Error) => log(e, LogLevel.Error))
